9019c843c0ceb5ac022a950a31cafcda85c5d8e4
BerndOlleckCap <bernd.olleck@capgemini.com>
2019-12-10 13:58:29 +0100

    added imperative programming sample solution

--- src/app/todo/services/dashboard/dashboard.service.ts
+++ src/app/todo/services/dashboard/dashboard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Observable, combineLatest, forkJoin} from 'rxjs';
+import {Observable, combineLatest, forkJoin, Subject} from 'rxjs';
 import {map, switchMap} from 'rxjs/operators';
 
 import { UserService } from '../user/user.service';
@@ -19,6 +19,10 @@ export class DashboardService {
   ) {}
 
   getTodosWithUsers(): Observable<TodoWithUser[]> {
+    return this.getTodosWithUsersMixture();
+  }
+
+  getTodosWithUsersMixture(): Observable<TodoWithUser[]> {
     return this.todoService.getTodos().pipe(
       switchMap((todos: Todo[]) => {
         const userIds: number[] = [... new Set(todos.map( (todo: Todo) => todo.userId))];
@@ -38,6 +42,59 @@ export class DashboardService {
         );
       })
     );
+  }
+
+  /**
+   * a solution with imperative programming
+   */
+  getTodosWithUsersImperative(): Observable<TodoWithUser[]> {
+    // this will be our actual result
+    const resultSubject = new Subject<TodoWithUser[]>();
+    // this is the array where we store the actual results
+    const result: TodoWithUser[] = [];
+    // this will cache the user data we already loaded
+    const userCache: {[userId: number]: User} = {};
+    // this will store the Todos where the user object has not been loaded yet, but it has already been requested
+    const pendingToDosByUserId: {[userId: number]: Todo[]} = {};
+
+    this.todoService.getTodos().subscribe( (todos: Todo[]) => {
+      todos.forEach((todo: Todo) => {
+        if (userCache[todo.userId]) {
+          const todoWithUser: TodoWithUser = { todo, user: userCache[todo.userId]};
+          result.push(todoWithUser);
+          if (result.length == todos.length) {
+            resultSubject.next(result);
+          }
+        } else {
+          // we don't have the user yet, store the Todo for later
+          let firstTodoForUserId = false;
+          if (!pendingToDosByUserId[todo.userId]) {
+            pendingToDosByUserId[todo.userId] = [];
+            firstTodoForUserId = true;
+          }
+          pendingToDosByUserId[todo.userId].push(todo);
+          // and if it was the first for the user id, request it
+          if (firstTodoForUserId) {
+            this.userService.getUser(todo.userId).subscribe((user: User) => {
+              // store in cache
+              userCache[user.id] = user;
+              // fill all queued todos
+              pendingToDosByUserId[user.id].forEach((previousTodo: Todo) => {
+                const todoWithUser: TodoWithUser = {todo: previousTodo, user};
+                result.push(todoWithUser);
+              });
+              delete pendingToDosByUserId[user.id];
+              // do I have now everything?
+              if (result.length == todos.length) {
+                resultSubject.next(result);
+              }
+            });
+          }
+        }
+      })
+    });
+    // The following happens way before the todos and their users are requested!
+    return resultSubject.asObservable();
   }
 
 }
