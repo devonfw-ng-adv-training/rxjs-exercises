bcc4b46f9359d8f7a9cb3d9d00b02bda241b5cfd
BerndOlleckCap <bernd.olleck@capgemini.com>
2020-09-13 14:46:04 +0200

    added level 7 with iif and timeoutWith

---
+++ src/app/levels/level7.service.ts
@@ -0,0 +1,44 @@
+import {Injectable} from '@angular/core';
+import {iif, Observable, of} from 'rxjs';
+import {debounceTime, distinctUntilChanged, switchMap, tap, timeoutWith} from 'rxjs/operators';
+import * as _ from 'lodash';
+
+export interface Backend {
+  getAutocompleteValues(input: string): Observable<Array<string>>;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Level7Service {
+
+  constructor() {
+  }
+
+  /**
+   * return an observable that produces an autocomplete list for the stream of inputs based on the
+   * given backend with the following requirements:
+   * <UL>
+   *   <LI>return an empty list if not at least 2 characters have been given
+   *       (instead of returning nothing - since it is not nice to display old results)</LI>
+   *   <LI>when the source changes rapidly (<500ms), do not load the data</LI>
+   *   <LI>do not return data if there is already a new input value before the previous input has been processed</LI>
+   *   <LI>do not return the same value twice in a row (hint: use _.isEqual(a,b) to compare arrays)</LI>
+   *   <LI>return an empty list if the backend does not respond within 1000ms.</LI>
+   * </UL>
+   */
+  getAutocompleteList(inputObservable$: Observable<string>, backend: Backend): Observable<Array<string>> {
+    return inputObservable$.pipe(
+      debounceTime(500),
+      switchMap(value =>
+        // note: the iif has to be wrapped within an higher order observable
+        // since the predicate is evaluated at subscription time
+        iif(
+          () => value && value.length >= 2,
+          backend.getAutocompleteValues(value).pipe(timeoutWith(1000, of([]))),
+          of([]))),
+      distinctUntilChanged((oldValue,newValue) => _.isEqual(oldValue, newValue))
+    );
+  }
+
+}
