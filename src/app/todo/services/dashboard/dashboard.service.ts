43a6a4d164b2c19496b998e99e66862845e7ba48
BerndOlleckCap <bernd.olleck@capgemini.com>
2022-04-05 09:48:35 +0200

    angular advanced: added task comment to rxjs challenge exercise in the DashboardService

--- src/app/todo/services/dashboard/dashboard.service.ts
+++ src/app/todo/services/dashboard/dashboard.service.ts
@@ -15,6 +15,12 @@ export class DashboardService {
     private todoService: TodoService
   ) {}
 
+  /**
+   * The purpose of this method is to return all todos with their associated user object.
+   * The todos are returned by the TodoService, the corresponding users by id from the UserService.
+   * But: While a user may have multiple todos, a user object should only be requested once.
+   * And the list of todos should be returned in the same order as they are received from the TodoService.
+   */
   getTodosWithUsers(): Observable<TodoWithUser[]> {
     // TODO call todoService to receive todos,
     // TODO and call userService after for every user id (but not twice for the same one)
